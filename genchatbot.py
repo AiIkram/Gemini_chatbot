9d6f107ba501ae4dff900db282e472c7c852eb2d
Aissiou Ikram <88051280+AiIkram@users.noreply.github.com>
2024-12-12 08:12:53 +0100

    Update genchatbot.py

--- genchatbot.py
+++ genchatbot.py
@@ -13,7 +13,7 @@ import joblib
 import streamlit as st
 import google.generativeai as genai
 from dotenv import load_dotenv
-load_dotenv()
+
 GOOGLE_API_KEY=os.environ.get('GOOGLE_API_KEY')
 genai.configure(api_key=GOOGLE_API_KEY)
 
